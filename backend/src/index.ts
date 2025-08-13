f98e1d10ef5bfcbee3cbf5c83052a2accf0d9163
otaviovcn <viinident@gmail.com>
2025-08-13 17:34:58 -0300

    :sparkles: feat: Initializes the server configuration

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+// I usually use this GET on the root endpoint to check if the server is working correctly
+app.get("/", (_, res) => {
+res.send('The server is working correctly!');
+});
+
+// This GET will allow you to scrape the Amazon page
+app.get("/api/scrape", (req, res) => {
+  res.send('Scraping functionality is not implemented yet.');
+});
+
+// Tells the server to start listening for requests
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
