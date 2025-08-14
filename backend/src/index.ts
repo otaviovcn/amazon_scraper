c002e08d263ea1da049a707689dce6d92bb07cc5
otaviovcn <viinident@gmail.com>
2025-08-14 15:33:07 -0300

    :bug: fix: Fixed 'api/scrape' route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,16 +14,23 @@ app.get("/", (_, res) => {
 app.get("/api/scrape", async (req, res) => {
   const { keyword } = req.query;
 
+  // If the 'keyword' parameter is not provided, an error message will be returned
+  if (!keyword) {
+    return res.status(400).json({ error: "Parameter 'keyword' is required" });
+  }
+
   try {
+    // The 'headers', which is an optional parameter, is used so that
+    //  when the GET request is made, it appears to be a browser request.
+    // The Amazon was blocking it and this is to avoid being blocked by Amazon's anti-scraping measures
     const response = await axios.get(
       `https://www.amazon.com/s?k=${keyword}`,
       {
-        // The 'headers', which is an optional parameter, is used so that
-        //  when the GET request is made, it appears to be a browser request.
         headers: {
           "User-Agent": "Mozilla/5.0"
         },
       });
+
     //This part is to make a copy of the original DOM, so that it is not 
     // necessary to have the page open to make the necessary manipulations
     const dom: JSDOM = new JSDOM(response.data);
@@ -34,14 +41,20 @@ app.get("/api/scrape", async (req, res) => {
 
     // Here will be the capture of relevant data
     document.querySelectorAll('.s-card-container').forEach((product) => {
-      const title = product.querySelector('h2')?.textContent.trim();
-      const image = document.querySelector('img')?.src;
-      const rating = document.querySelector('.mvt-review-star-mini-popover')?.
-        getAttribute('aria-label')?.trim();
-      const reviews = product.querySelector('.puis-normal-weight-text')?.textContent.trim();
 
-      if (title && image && rating && reviews) {
-        results.push({ title, image, rating, reviews });
+      /* "The request wasn't getting much success with these two selectors below, but 
+      the AI suggested creating "more resilient" selectors to try to escape Amazon's analysis"
+      -> const rating = document.querySelector('.mvt-review-star-mini-popover')?.getAttribute('aria-label')?.trim();
+      -> const reviews = product.querySelector('.puis-normal-weight-text')?.textContent.trim();*/
+
+      const title: string | undefined = product.querySelector('h2')?.textContent?.trim();
+      const image: string | undefined = product.querySelector('img')?.src;
+      const rating: string | undefined = product.querySelector('[aria-label*="out of 5 stars"]')?.textContent?.trim();
+      const reviews: string | undefined = product.querySelector('.s-link-style .s-underline-text')?.textContent?.trim();
+
+
+      if (title && image) {
+        results.push({ title, image, rating: rating || "N/A", reviews: reviews || "N/A" });
       }
     });
 
